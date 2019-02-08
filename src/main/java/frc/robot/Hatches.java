8e8734df79547ea7f3441c9ff49a6762fd698c8d
Unknown <ant.iatonna@gmail.com>
2019-02-07 20:39:14 -0500

    Hatches Recreated

---
+++ src/main/java/frc/robot/Hatches.java
@@ -0,0 +1,46 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+/**
+ * Add your docs here.
+ */
+public class Hatches {
+
+    static VictorSPX right;
+    static VictorSPX left;
+
+    public static void Hatch() {
+        
+   VictorSPX right = new VictorSPX(RobotMap.rightHatch);
+   VictorSPX left = new VictorSPX(RobotMap.leftHatch);
+
+     }
+    public static void HatchesIn() {
+
+        right.set(ControlMode.PercentOutput,0.5);
+        left.set(ControlMode.PercentOutput,-0.5);
+
+    }
+   
+    public static void HatchesOut() {
+
+        right.set(ControlMode.PercentOutput,-0.5);
+        left.set(ControlMode.PercentOutput,0.5);
+
+}
+    public static void HatchesStop() {
+
+        right.set(ControlMode.PercentOutput,0);
+        left.set(ControlMode.PercentOutput,0);
+
+}
+}
+
+//D@nte wants to break free from the shackles of society
