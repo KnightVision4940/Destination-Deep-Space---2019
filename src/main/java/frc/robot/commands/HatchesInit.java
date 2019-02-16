78cbc40e379ea58d9b4b4dfc6e96e65de42df8e7
Unknown <gabriel.i.rueda@gmail.com>
2019-02-16 10:53:23 -0500

    Fixes

--- src/main/java/frc/robot/commands/HatchesInit.java
+++ src/main/java/frc/robot/commands/HatchesInit.java
@@ -43,6 +43,7 @@ public class HatchesInit extends Command {
   // Called once after isFinished returns true
   @Override
   protected void end() {
+    Robot.H.HatchesStop();
   }
 
   // Called when another command which requires one or more of the same
