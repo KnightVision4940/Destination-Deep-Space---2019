7e3fa6c006f11278999f762b8583371f9450077d
Unknown <ddagati@hotmail.ca>
2019-02-16 10:32:06 -0500

    climb victors

--- src/main/java/frc/robot/subsystems/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -8,7 +8,7 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import frc.robot.RobotMap;
 import frc.robot.subsystems.DriveTrain;
@@ -18,9 +18,11 @@ import frc.robot.subsystems.DriveTrain;
 public class Climb extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
-  static TalonSRX climbMotor = new TalonSRX(RobotMap.climbmotor);
+  static VictorSPX climbMotor = new VictorSPX (RobotMap.climbmotor);
 
     public static void climb(){
+
+      
     
     }
   @Override
